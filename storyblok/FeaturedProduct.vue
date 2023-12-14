085b3909ae61d8c6cbc58f96c1c47baeecde9f9c
Florian Hitz <web.florianhitz@gmail.com>
2023-12-14 17:09:39 +0100

    updatet featuredProduct

--- storyblok/FeaturedProduct.vue
+++ storyblok/FeaturedProduct.vue
@@ -3,6 +3,7 @@
     :to="`/products/${product.slug}`"
     v-editable="blok"
     class="featured-product flex items-center justify-center flex-col my-24 container mx-auto"
+    v-if="product && !product.content.sold"
   >
     <NuxtImg
       :src="product.content.productImage.filename"
