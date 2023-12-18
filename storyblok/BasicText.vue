492b27f1506c814e89cc165b0b990aa4189ee858
Florian Hitz <florian@raffiniert.biz>
2023-12-18 19:56:21 +0100

    added a basicText Component

---
+++ storyblok/BasicText.vue
@@ -0,0 +1,10 @@
+<template>
+    <div v-html="resolvedRichText" class="basic-text">
+    </div>
+</template>
+  
+<script setup>
+const props = defineProps({ blok: Object });
+const resolvedRichText = computed(() => renderRichText(props.blok.text));
+</script>
+  
