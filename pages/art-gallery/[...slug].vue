8f1830c5b97bf122c270a2c2a6afabd075ad7d1d
Florian Hitz <web.florianhitz@gmail.com>
2024-01-23 22:28:43 +0100

    improved SEO

--- pages/art-gallery/[...slug].vue
+++ pages/art-gallery/[...slug].vue
@@ -3,6 +3,26 @@ const { slug } = useRoute().params;
 const story = await useAsyncStoryblok("art-gallery/" + slug.join("/"), {
   version: "draft",
 });
+
+let description = '';
+
+//loop over the description and split by ,
+for (let i = 0; i < story.value.content.description.content.length; i++) {
+  //loop over the content of the description and split by ,
+  for(let x = 0; x < story.value.content.description.content[i].content.length; x++) {
+    //if the text is not empty, add it to the description
+    if(story.value.content.description.content[i].content[x].text) {
+      description += story.value.content.description.content[i].content[x].text;
+    }
+  }
+
+  description += ". ";
+}
+
+useServerSeoMeta({
+  title: () => story.value.content.name,
+  description: () => description
+});
 </script>
 
 <template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <script setup>
 const story = await useAsyncStoryblok('home', { version: 'draft' });
+
+useServerSeoMeta({
+  title: () => story.value.content.Meta.title,
+  description: () => story.value.content.Meta.description,
+  ogTitle: () => story.value.content.Meta.ogTitle,
+  ogDescription: () => story.value.content.Meta.ogDescription,
+});
 </script>
 
 <template>
